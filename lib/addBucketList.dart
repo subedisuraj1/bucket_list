7a9dbbf3a93f245b6e062d5a9620f35ec707442b
subedisuraj1 <sr.subedi1@gmail.com>
2024-12-27 19:49:17 +0545

    completed

--- lib/addBucketList.dart
+++ lib/addBucketList.dart
@@ -2,7 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class AddBucketList extends StatefulWidget {
-  int newIndex;
+  final int newIndex;
   AddBucketList({super.key, required this.newIndex});
 
   @override
@@ -33,35 +33,78 @@ class _AddBucketListState extends State<AddBucketList> {
 
   @override
   Widget build(BuildContext context) {
+    var addForm = GlobalKey<FormState>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Add Bucket List"),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Column(
-          spacing: 30,
-          children: [
-            TextField(
-              controller: itemText,
-              decoration: InputDecoration(label: Text("Item")),
-            ),
-            TextField(
-              controller: costText,
-              decoration: InputDecoration(label: Text("Estimated Cost")),
-            ),
-            TextField(
-              controller: imageURLText,
-              decoration: InputDecoration(label: Text("Image Url")),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  ElevatedButton(onPressed: addData, child: Text("Add Item"))
-                ],
+        child: Form(
+          key: addForm,
+          child: Column(
+            children: [
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "must be more than 3 character";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                },
+                controller: itemText,
+                decoration: InputDecoration(label: Text("Item")),
               ),
-            )
-          ],
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "must be more than 3 character";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                },
+                controller: costText,
+                decoration: InputDecoration(label: Text("Estimated Cost")),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) {
+                  if (value.toString().length < 3) {
+                    return "must be more than 3 character";
+                  }
+                  if (value == null || value.isEmpty) {
+                    return "This must not be empty";
+                  }
+                },
+                controller: imageURLText,
+                decoration: InputDecoration(label: Text("Image Url")),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Container(
+                width: double.infinity,
+                child: ElevatedButton(
+                    onPressed: () {
+                      if (addForm.currentState!.validate()) {
+                        addData();
+                      }
+                    },
+                    child: Text("Add Item")),
+              )
+            ],
+          ),
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
      home: MainScreen(),
       debugShowCheckedModeBanner: false,
     );
